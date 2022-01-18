b23b665d2b00634d148c8fddf7a4b93d0bf9a476
Prajwol Shrestha <70046321+Prajwol-Shrestha@users.noreply.github.com>
2022-01-18 13:37:21 +0545

    Manga List

---
+++ Manga List/mangas.js
@@ -0,0 +1,75 @@
+// Dynamic Ratings Left To Built
+
+
+mangas = [
+	{
+		id: 0,
+		name: 'Above Ten Thousand People',
+		img: './images/aboveTenThousandPeople.jpg',
+		tags: 'Cultivation, Comedy, Reincarnation',
+		translation: 'Excellent',
+	},
+
+	{
+		id: 1,
+		name: 'Martial Peak',
+		img: './images/martialPeak.jpg',
+		tags: 'Cultivation, Martial Arts',
+		translation: 'Excellent',
+	},
+
+	{
+		id: 2,
+		name: 'Apotheosis',
+		img: './images/apotheosis.jpg',
+		tags: 'Cultivation, Martial Arts',
+		translation: 'Excellent',
+	},
+
+	{
+		id: 3,
+		name: 'Demon Magic Emperor',
+		img: './images/magicEmperor.jpg',
+		tags: 'Cultivation, Martial Arts, Reincarnation',
+		translation: 'Excellent',
+	},
+
+
+	{
+		id: 4,
+		name: 'Tales Of Demons And Gods',
+		img: './images/talesOfDemonsAndGods.jpg',
+		tags: 'Cultivation, Martial Arts, Reincarnation',
+		translation: 'Excellent',
+	},
+
+
+	{
+		id: 5,
+		name: 'The Constellation That Returned From Hell',
+		img: './images/theConstellationThatReturnedFromHell.jpg',
+		tags: 'Cultivation, Martial Arts ',
+		translation: 'Excellent',
+	},
+
+
+	{
+		id: 6,
+		name: 'Wandering Warrior of Wudang',
+		img: './images/wanderingWarriorOfWudang.jpg',
+		tags: 'Cultivation, Martial Arts, Reincarnation',
+		translation: 'Excellent',
+	},
+
+
+	{
+		id: 7,
+		name: "Player That Can't Level Up",
+		img: './images/playerThatCannotLevelUp.jpg',
+		tags: '',
+		translation: 'Excellent',
+	},
+
+
+
+]
---
+++ Manga List/script.js
@@ -0,0 +1,37 @@
+// Selecting Elements
+
+const flexEl = document.querySelector('.flexContainer');
+
+
+// Render manga ID Cards
+
+function renderIdCards(){
+	mangas.forEach( manga => {
+		flexEl.innerHTML += `
+			<div class="cardContainer floating">
+			<div class="cardContent">
+				<div class="imgContainer">
+					<img src= ${manga.img}>	
+				</div>
+		
+ 				<div class="stdDetails">
+ 					<ul>
+ 						<li> <span> Name :	<!-- &nbsp; --> </span> ${manga.name} </li>
+	 					<li> <span> Tags 	: </span> ${manga.tags} </li>
+	 					<li> <span> Translation : </span> ${manga.translation} </li>
+	 					<li> Rating: <span class="fa fa-star checked"></span>
+							<span class="fa fa-star checked"></span>
+							<span class="fa fa-star checked"></span>
+							<span class="fa fa-star"></span>
+							<span class="fa fa-star"></span>
+						</li>
+	 				</ul>
+ 				</div>
+			</div>
+		</div>
+		`
+	})
+}
+
+renderIdCards();
+

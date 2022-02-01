9cc9907f9a76cd0ef24fe4c8d31652bf37dce8ec
Prajwol Shrestha <70046321+Prajwol-Shrestha@users.noreply.github.com>
2022-02-01 21:29:47 +0545

    Added Image Slider

---
+++ Image Slider/script.js
@@ -0,0 +1,45 @@
+const imgEl = document.querySelector('img');
+
+const nextEl = document.querySelector('#next');
+const previousEl = document.querySelector('#previous');
+
+
+
+const imgNames = [
+	"magicEmperor",
+	"aboveTenThousandPeople",
+	"cultivatorAgainstHeroSociety",
+	"apotheosis",
+	"returnOfMountHuaSect",
+	"talesOfDemonsAndGods",
+	"martialPeak",
+]
+
+
+
+
+let i = 0;
+
+nextEl.onclick = () => {
+	i++;
+	if(i < imgNames.length){
+		imgEl.setAttribute('src', `./images/${imgNames[i]}.jpg`)
+	}
+	else{
+		i = 0; 
+		imgEl.setAttribute('src', `./images/${imgNames[i]}.jpg`)
+	}
+};
+previousEl.onclick = () => {
+	i--;
+	if(i < 0){
+		i = imgNames.length - 1;
+		imgEl.setAttribute('src', `./images/${imgNames[i]}.jpg`)
+	}
+	else{
+		imgEl.setAttribute('src', `./images/${imgNames[i]}.jpg`)
+	}
+};
+
+
+

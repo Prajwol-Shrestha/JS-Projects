a647a00ff9f9d4dc32fb3be54f646557fb864e8e
Prajwol Shrestha <70046321+Prajwol-Shrestha@users.noreply.github.com>
2022-02-01 21:40:46 +0545

    Added Files

--- Image Slider/script.js
+++ Image Slider/script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const imgEl = document.querySelector('img');
 
 const nextEl = document.querySelector('#next');
@@ -15,7 +14,6 @@ const imgNames = [
 	"talesOfDemonsAndGods",
 	"martialPeak",
 ]
-
 
 
 
@@ -42,52 +40,3 @@ previousEl.onclick = () => {
 	}
 };
 
-
-
-=======
-const imgEl = document.querySelector('img');
-
-const nextEl = document.querySelector('#next');
-const previousEl = document.querySelector('#previous');
-
-
-
-const imgNames = [
-	"magicEmperor",
-	"aboveTenThousandPeople",
-	"cultivatorAgainstHeroSociety",
-	"apotheosis",
-	"returnOfMountHuaSect",
-	"talesOfDemonsAndGods",
-	"martialPeak",
-]
-
-
-
-
-let i = 0;
-
-nextEl.onclick = () => {
-	i++;
-	if(i < imgNames.length){
-		imgEl.setAttribute('src', `./images/${imgNames[i]}.jpg`)
-	}
-	else{
-		i = 0; 
-		imgEl.setAttribute('src', `./images/${imgNames[i]}.jpg`)
-	}
-};
-previousEl.onclick = () => {
-	i--;
-	if(i < 0){
-		i = imgNames.length - 1;
-		imgEl.setAttribute('src', `./images/${imgNames[i]}.jpg`)
-	}
-	else{
-		imgEl.setAttribute('src', `./images/${imgNames[i]}.jpg`)
-	}
-};
-
-
-
->>>>>>> e79fd32... Added Image Slider
